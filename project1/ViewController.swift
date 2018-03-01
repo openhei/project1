2cb3f30c2598e660bfad21b7632478a4e139a6d6
openhei <qweb777@gmail.com>
2018-03-01 18:54:01 -0500

    hello

--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -29,6 +29,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
      
         playAnimation()
+        
+        // hello 
    
     }
 
